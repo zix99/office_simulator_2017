0a8792e38188d05fc6d333a0b4de99f2b36259d1
Christopher LaPointe <chlapointe@vistaprint.com>
2016-03-25 20:12:52 -0400

    Before refactoring

--- static/game.js
+++ static/game.js
@@ -1,15 +1,30 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 var characters = {
-
+    'chris' : {
+        image: "officeman2",
+        name: "Chris"
+    },
+    'caleb' : {
+        image: "russian_f1"
+    }
 };
+
+var PLAYER_SPEED = 200;
+
+var player = null;
+var map = null;
+
+function getPlayerName() {
+    return "chris";
+}
 
 function preloadSprite(name, img) {
 	return game.load.spritesheet(name, 'assets/characters/' + img + '.png', 32, 48, 16);
 }
 
-function createSprite(name) {
-	var sprite = game.add.sprite(0, 0, name);
+function createSprite(key, person) {
+	var sprite = person._sprite = game.add.sprite(0, 0, key);
 	sprite.anchor.setTo(0.5, 0);
 	sprite.scale.setTo(1.25);
 
@@ -20,24 +35,28 @@ function createSprite(name) {
 	sprite.animations.add('walk_right', [8,9,10,11], fps, true);
 	sprite.animations.add('walk_up', [12,13,14,15], fps, true);
 
+    game.physics.p2.enable(sprite);
+    
+    if (person.name) {
+        var nameSprite = game.add.text(0,0, person.name);
+        sprite.addChild(nameSprite);
+    }
+
 	return sprite;
 }
 
 function preload() {
 	game.add.plugin(Phaser.Plugin.Tiled);
-    //  You can fill the preloader with as many assets as your game requires
 
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    //game.load.image('einstein', 'assets/tiles/indoors.png');
     var cacheKey = Phaser.Plugin.Tiled.utils.cacheKey;
 
     game.load.tiledmap(cacheKey('tiled-lvl0', 'tiledmap'), 'assets/levels/test.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image(cacheKey('tiled-lvl0', 'tileset', 'terrain_atlas'), 'assets/tiles/terrain_atlas.png');
     
-    preloadSprite('player', 'officeman2');
+    _.each(characters, function(val, key) {
+        console.log(val);
+        val._sprite = preloadSprite(key, val.image);
+    });
 }
 
 function create() {
@@ -45,45 +64,55 @@ function create() {
     //  This creates a simple sprite that is using our loaded image and
     //  displays it on-screen
     //game.add.sprite(0, 0, 'einstein');
-    var map = game.add.tiledmap('tiled-lvl0');
-/*
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-    player.anchor.setTo(0.5, 0.5);
-    player.scale.setTo(1.5,1.5);
+    map = game.add.tiledmap('tiled-lvl0');
 
-    player.animations.add('stand', [0]);
-    player.animations.add('walk_down', [0,1,2,3], 8, true);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.convertTiledCollisionObjects(map, 'collision');
 
-    player.animations.play('walk_down');*/
-
-    player = createSprite('player');
+    var playerName = getPlayerName();
+    _.each(characters, function(val, key) {
+        var sprite = createSprite(key, val);
+        if (key === playerName) {
+            player = sprite;
+        }
+    });
+    if (player) {
+        //game.physics.p2.enable(player);
+        game.camera.follow(player);
+    }
 }
 
 function update() {
+    //Update player sprite with movement
+    player.body.setZeroVelocity();
+    player.body.setZeroRotation();
+    //player.body.setZeroForce();
+    player.body.rotation = 0;
+
 	var moving = false;
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    if (game.input.keyboard.isDown(Phaser.Keyboard.A))
     {
-        player.x -= 4;
+        player.body.velocity.x -= PLAYER_SPEED;
         player.animations.play('walk_left');
         moving = true;
     }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
     {
-        player.x += 4;
+        player.body.velocity.x += PLAYER_SPEED;
         player.animations.play('walk_right');
         moving = true;
     }
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W))
     {
-        player.y -= 4;
+        player.body.velocity.y -= PLAYER_SPEED;
         player.animations.play('walk_up');
         moving = true;
     }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
     {
-        player.y += 4;
+        player.body.velocity.y += PLAYER_SPEED;
         player.animations.play('walk_down');
         moving = true;
     }
@@ -94,5 +123,6 @@ function update() {
 }
 
 function render() {
-
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 32, 500);
 }
